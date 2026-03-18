a174854de83f54e3ab0658c169bc218f2a952f46
sjbtherapy247 <sjbtherapy247@gmail.com>
2026-03-18 11:05:24 +1100

    re-enable BackNav + region fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-// import BackNav from '@/components/BackNav';
+import BackNav from '@/components/BackNav';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <BackNav /> */}
+        <BackNav />
         {children}
         <Analytics />
         <SpeedInsights />
